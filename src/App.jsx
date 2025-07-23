fbcbc0278bab7cccf0d22aedd3321f7e6df57613
sandilyakoppolu <sandilyakoppolu007@gmail.com>
2025-07-23 09:55:07 +0530

    22

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/CICD">
+    <Router basename="/s09">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
